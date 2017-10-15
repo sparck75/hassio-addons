862ada6f4111470e22c3cb1a0b05304f8ed39e4f
sparck75 <dominic.lavoie@gmail.com>
2017-10-15 19:58:48 -0400

    changed bash variables

--- appdaemon/run.sh
+++ appdaemon/run.sh
@@ -3,17 +3,16 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CONFIG_DIR=/config/hadaemon
-EXTRA_CMD="-D DEBUG"
+
 
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
 
-if [ ! -d ${CONFIG_DIR} ]; then
-	mkdir -p ${CONFIG_DIR}
-	cp /etc/appdaemon-example.yaml ${CONFIG_DIR}/appdaemon.yaml
+if [ ! -d $CONFIG_DIR ]; then
+	mkdir -p $CONFIG_DIR
+	cp /etc/appdaemon-example.yaml $CONFIG_DIR/appdaemon.yaml
 fi
 
-echo 0.0.0.0 ${DOMAIN} >> /etc/hosts
-
+echo 0.0.0.0 $DOMAIN >> /etc/hosts
 echo "Starting appdaemon from hassio-addons"
-appdaemon -c "$CONFIG_DIR" "$EXTRA_CMD"
 
+appdaemon -c $CONFIG_DIR $EXTRA_CMD
