e2ea13a74f4ce316ef77450bec88ecea407d2564
sparck75 <dominic.lavoie@gmail.com>
2017-10-15 20:43:39 -0400

    debug

--- appdaemon/run.sh
+++ appdaemon/run.sh
@@ -14,5 +14,10 @@ fi
 
 echo 0.0.0.0 $DOMAIN >> /etc/hosts
 echo "Starting appdaemon from hassio-addons"
+echo "Config: " $CONFIG_DIR
+echo "Extra: " $EXTRA_CMD
+echo "Domain: " $DOMAIN
+echo pwd
+echo ls -l
 
 appdaemon -c $CONFIG_DIR $EXTRA_CMD
