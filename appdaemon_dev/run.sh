e966c2cb112b0834f2da7faea46cd416336f2c80
sparck75 <dominic.lavoie@gmail.com>
2017-10-16 21:31:03 -0400

    fix

--- appdaemon_dev/run.sh
+++ appdaemon_dev/run.sh
@@ -3,21 +3,17 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CONFIG_DIR=/config/hadaemon
-
+EXTRA_CMD="-D DEBUG"
 
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
 
 if [ ! -d $CONFIG_DIR ]; then
 	mkdir -p $CONFIG_DIR
 	cp /etc/appdaemon-example.yaml $CONFIG_DIR/appdaemon.yaml
+	cp /etc/apps-example.yaml $CONFIG_DIR/apps.yaml
 fi
 
 echo 0.0.0.0 $DOMAIN >> /etc/hosts
-echo "Starting appdaemon from hassio-addons"
-echo "Config: " $CONFIG_DIR
-echo "Extra: " $EXTRA_CMD
-echo "Domain: " $DOMAIN
-echo pwd
-echo ls -l
 
 appdaemon -c $CONFIG_DIR $EXTRA_CMD
+
