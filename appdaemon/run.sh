dde9b4de4643d762b7dd91acd62c107eae9d75f7
sparck75 <dominic.lavoie@gmail.com>
2017-10-18 14:49:27 -0400

    fix option

--- appdaemon/run.sh
+++ appdaemon/run.sh
@@ -3,9 +3,6 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CONFIG_DIR=/config/hadaemon
-EXTRA_CMD=""
-EXTRA_CMD2=""
-
 
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
 COMMTYPE=$(jq --raw-output ".commtype" $CONFIG_PATH)
@@ -17,15 +14,32 @@ if [ ! -d $CONFIG_DIR ]; then
 	cp /etc/apps-example.yaml $CONFIG_DIR/apps.yaml
 fi
 
-if [ ! $DEBUGGING == "DEBUG" ]; then
-	EXTRA_CMD = "-D DEBUG"
+
+if [ $DEBUGGING = "debug" ]; then
+  EXTRA_CMD="-D DEBUG"
+elif [ $DEBUGGING = "info" ]; then
+  EXTRA_CMD="-D INFO"
+elif [ $DEBUGGING = "warning" ]; then
+  EXTRA_CMD="-D WARNING"
+elif [ $DEBUGGING = "error" ]; then
+  EXTRA_CMD="-D ERROR"
+elif [ $DEBUGGING = "critical" ]; then
+  EXTRA_CMD="-D CRITICAL"
+elif [ $DEBUGGING = "none" ]; then
+  EXTRA_CMD=" "
+else
+  echo "Invalid debugging option in config.json.\n Valid value are: debug, info, warning, error, critical, none"
+  EXTRA_CMD=" "
 fi
 
-if [ $COMMTYPE == "SSE" ]; then
-	EXTRA_CMD2 = "--commtype:SSE "
+if [ $COMMTYPE == "sse" ]; then
+  EXTRA_CMD2="--commtype=SSE "
+elif [ $COMMTYPE == "websockets" ]; then
+  EXTRA_CMD2="--commtype=WEBSOCKETS "  
+else
+  echo "Invalid commtype option in config.json.\n (Valid value are: websockets or sse"
+  EXTRA_CMD2=" "
 fi
-
-
 echo 0.0.0.0 $DOMAIN >> /etc/hosts
 
 appdaemon -c $CONFIG_DIR $EXTRA_CMD $EXTRA_CMD2
