9c9354f50ba0d48a6ef0ec4ff984ed100b710853
sparck75 <dominic.lavoie@gmail.com>
2017-10-17 23:10:04 -0400

    fix 1.2

--- appdaemon_dev/run.sh
+++ appdaemon_dev/run.sh
@@ -3,9 +3,13 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CONFIG_DIR=/config/hadaemon
-EXTRA_CMD="-D DEBUG --commtype=SSE"
+EXTRA_CMD=""
+EXTRA_CMD2=""
+
 
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
+COMMTYPE=$(jq --raw-output ".commtype" $CONFIG_PATH)
+DEBUGGING=$(jq --raw-output ".debugging" $CONFIG_PATH)
 
 if [ ! -d $CONFIG_DIR ]; then
 	mkdir -p $CONFIG_DIR
@@ -13,9 +17,15 @@ if [ ! -d $CONFIG_DIR ]; then
 	cp /etc/apps-example.yaml $CONFIG_DIR/apps.yaml
 fi
 
-echo 0.0.0.0 $DOMAIN >> /etc/hosts
-echo "Running dev branch of appdaemon"
-echo "Use at your own risk"
-appdaemon -v
-appdaemon -c $CONFIG_DIR $EXTRA_CMD
+if [ ! $DEBUGGING == "DEBUG" ]; then
+	$EXTRA_CMD = "-D DEBUG"
+fi
 
+if [ $COMMTYPE == "SSE" ]; then
+	$EXTRA_CMD2 = "--commtype:SSE "
+fi
+
+
+echo 0.0.0.0 $DOMAIN >> /etc/hosts
+
+appdaemon -c $CONFIG_DIR $EXTRA_CMD $EXTRA_CMD2
